bd4cf6dc2975fbf1e76c27c0b6e54ecc84800d90
ejioforcelestine <ejioforcelestine77@gmail.com>
2024-03-08 10:55:26 +0100

    added connect to github button

---
+++ convex/schema.ts
@@ -0,0 +1,21 @@
+import { defineSchema, defineTable } from 'convex/server'
+import { v } from 'convex/values'
+export default defineSchema({
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    installationId: v.string(),
+    role: v.string()
+  }),
+  projects: defineTable({
+    name: v.string(),
+    description: v.string(),
+    userId: v.string()
+  }),
+  issues: defineTable({
+    title: v.string(),
+    description: v.string(),
+    projectId: v.string(),
+    userId: v.string()
+  }),
+});
