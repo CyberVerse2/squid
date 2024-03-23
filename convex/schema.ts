8dd2db2eb6e29d3f572c167478796b1bde992bb2
ejioforcelestine <ejioforcelestine77@gmail.com>
2024-03-23 04:08:44 +0100

    fixed error

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,7 +2,7 @@ import { defineSchema, defineTable } from 'convex/server';
 import { v } from 'convex/values';
 export default defineSchema({
   users: defineTable({
-    name: v.string(),
+    name: v.optional(v.string()),
     githubUsername: v.optional(v.string()),
     clerkId: v.string(),
     email: v.string(),
